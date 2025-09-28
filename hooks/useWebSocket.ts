508ca71e2af4386c06a2497b0e1eb3acf50a52a1
Asahi <bihruzen@gmail.com>
2025-09-28 22:59:57 +0300

    Remove wagmi import from useWebSocket hook

--- hooks/useWebSocket.ts
+++ hooks/useWebSocket.ts
@@ -1,11 +1,12 @@
 'use client'
 
 import { useEffect, useState, useCallback } from 'react'
-import { useAccount } from 'wagmi'
+// import { useAccount } from 'wagmi'
 import { websocketService } from '@/lib/websocket'
 
 export function useWebSocket() {
-  const { address } = useAccount()
+  // const { address } = useAccount()
+  const address = '0x1234...5678' // Mock address
   const [isConnected, setIsConnected] = useState(false)
   const [marketData, setMarketData] = useState<any>({})
   const [traderPerformance, setTraderPerformance] = useState<any>({})
